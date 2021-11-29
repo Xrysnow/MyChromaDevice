e74356a5b553b9c6b76ac6b8409002f80ff939bc
Xrysnow <xrysnow@outlook.com>
2021-11-29 16:05:29 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import time
 import Device
 from HID import CTX12E4
 
+def smooth(v1, v2, alpha):
+    return v1 * (1 - alpha) + v2 * alpha
+
 class Handler:
     def __init__(self):
         self._t = time.time()
@@ -10,22 +13,27 @@ class Handler:
         self._b = 0
         self._keyboard = CTX12E4.create()
 
-    def handle(self, colors):
-        if time.time() - self._t < 0.1:
-            return
-        self._t = time.time()
-        r_, g_, b_ = colors[0]
+    def update(self, colors):
         alpha = 0.5
-        self._r = self._r * (1 - alpha) + r_ * alpha
-        self._g = self._g * (1 - alpha) + g_ * alpha
-        self._b = self._b * (1 - alpha) + b_ * alpha
+        r, g, b = list(map(lambda *a: sum(a) / len(a), *colors))
+        self._r = smooth(self._r, r, alpha)
+        self._g = smooth(self._g, g, alpha)
+        self._b = smooth(self._b, b, alpha)
+
+    def handle(self, colors):
+        # 30Hz => 10Hz
+        t = time.time()
+        if t - self._t < 0.1:
+            return
+        self._t = t
+        self.update(colors)
         if self._keyboard:
             self._keyboard.setQMKColorRGB(self._r, self._g, self._b)
 
 def main():
     # Ctrl+C to exit
     hdl = Handler()
-    conn = Device.DeviceConnector(Device.Headset, hdl.handle)
+    conn = Device.DeviceConnector(Device.Chromalink, hdl.handle)
     conn.start()
     conn.close()
     print('end')
